3652a79e9c53e6c7d97870b7aeec2b32f66ef103
simonleitl <simon.leitl@t-online.de>
2018-11-14 12:58:30 +0100

    Registration added

--- app/src/main/java/com/example/linkn/myapplication/MainActivity.java
+++ app/src/main/java/com/example/linkn/myapplication/MainActivity.java
@@ -1,18 +1,91 @@
 package com.example.linkn.myapplication;
 
-import android.support.v7.app.AppCompatActivity;
+import android.content.Intent;
 import android.os.Bundle;
+import android.support.annotation.NonNull;
+import android.support.v7.app.AppCompatActivity;
 import android.text.InputType;
+import android.text.TextUtils;
 import android.view.View;
+import android.widget.Button;
 import android.widget.EditText;
+import android.widget.ProgressBar;
+import android.widget.Toast;
+
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
 
 public class MainActivity extends AppCompatActivity {
 
+    public EditText emailTextbox, passwortTextBox;
+    public Button registrieren;
+    public FirebaseAuth auth;
+    private ProgressBar progressBar;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.start);
+
+
+
+
+
+
+    }
+
+
+    public void onClick(View view){
+        auth = FirebaseAuth.getInstance();
+
+        progressBar = (ProgressBar) findViewById(R.id.progressBar);
+        emailTextbox = (EditText) findViewById(R.id.emailText);
+        passwortTextBox = (EditText) findViewById(R.id.passwortTextBox);
+        registrieren=(Button) findViewById(R.id.registrierenButton);
+
+        String email = emailTextbox.getText().toString().trim();
+        String password = passwortTextBox.getText().toString().trim();
+
+        if (TextUtils.isEmpty(email)) {
+            Toast.makeText(getApplicationContext(), "Enter email address!", Toast.LENGTH_SHORT).show();
+            return;
+        }
+
+        if (TextUtils.isEmpty(password)) {
+            Toast.makeText(getApplicationContext(), "Enter password!", Toast.LENGTH_SHORT).show();
+            return;
+        }
+
+        if (password.length() < 6) {
+            Toast.makeText(getApplicationContext(), "Password too short, enter minimum 6 characters!", Toast.LENGTH_SHORT).show();
+            return;
+        }
+
+        progressBar.setVisibility(View.VISIBLE);
+        //create User
+
+        auth.createUserWithEmailAndPassword(email, password)
+                .addOnCompleteListener(MainActivity.this, new OnCompleteListener<AuthResult>() {
+                    @Override
+                    public void onComplete(@NonNull Task<AuthResult> task) {
+
+                        Toast.makeText(MainActivity.this, "createUserWithEmail:onComplete:" + task.isSuccessful(), Toast.LENGTH_SHORT).show();
+                          progressBar.setVisibility(View.GONE);
+                        // If sign in fails, display a message to the user. If sign in succeeds
+                        // the auth state listener will be notified and logic to handle the
+                        // signed in user can be handled in the listener.
+                        if (!task.isSuccessful()) {
+                            Toast.makeText(MainActivity.this, "Authentication failed." + task.getException(),
+                                    Toast.LENGTH_SHORT).show();
+                        } else {
+                            startActivity(new Intent(MainActivity.this, MainActivity.class));
+                            finish();
+                        }
+
+                    }
+                });
     }
 
     // eventListener für den Login Button auf der Startseite. Leitet zum Login weiter
@@ -63,4 +136,8 @@ public class MainActivity extends AppCompatActivity {
     public void withoutLoginLabelKlick(View view){
 
     }
-}
+          protected void onResume() {
+               super.onResume();
+               //progressBar.setVisibility(View.GONE);
+
+}}
