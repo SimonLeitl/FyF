b854a33878e80753f72077f8bf3cee48d2a2eb6e
simonleitl <simon.leitl@t-online.de>
2018-11-29 09:53:21 +0100

    added LoginActivity layout changes in login.xml , registration.xml

--- app/src/main/java/com/example/linkn/myapplication/Sucessful.java
+++ app/src/main/java/com/example/linkn/myapplication/Sucessful.java
@@ -1,8 +1,10 @@
 package com.example.linkn.myapplication;
 
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.view.View;
 
 public class Sucessful extends AppCompatActivity {
 
@@ -11,10 +13,9 @@ public class Sucessful extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.sucessful);
 
+    }
 
-
-
-
-
+    public void okButtonClick(View view){
+        startActivity(new Intent(Sucessful.this, MainActivity.class));
     }
 }
