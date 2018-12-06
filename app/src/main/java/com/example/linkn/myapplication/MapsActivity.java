9b183c0bb3f98a16664217c80624612f93a81323
MJ7898 <40462862+MJ7898@users.noreply.github.com>
2018-12-06 11:41:23 +0100

    create MapsActivity.java and map.xml

---
+++ app/src/main/java/com/example/linkn/myapplication/MapsActivity.java
@@ -0,0 +1,39 @@
+package com.example.linkn.myapplication;
+
+import android.annotation.SuppressLint;
+import android.location.Location;
+import android.location.LocationManager;
+import android.os.Bundle;
+import android.support.v4.app.FragmentActivity;
+
+import com.google.android.gms.maps.CameraUpdateFactory;
+import com.google.android.gms.maps.GoogleMap;
+import com.google.android.gms.maps.OnMapReadyCallback;
+import com.google.android.gms.maps.SupportMapFragment;
+import com.google.android.gms.maps.model.LatLng;
+import com.google.android.gms.maps.model.MarkerOptions;
+
+public class MapsActivity extends FragmentActivity implements OnMapReadyCallback {
+
+    private GoogleMap mMap;
+    private LocationOfDevice locationOfDevice;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.map);
+
+        SupportMapFragment mapFragment = (SupportMapFragment) getSupportFragmentManager().findFragmentById(R.id.map);
+        mapFragment.getMapAsync(this);
+    }
+
+
+    @Override
+    public void onMapReady(GoogleMap googleMap) {
+        mMap = googleMap;
+
+        locationOfDevice = new LocationOfDevice();
+        // Add a marker on the Position of the user
+
+    }
+}
