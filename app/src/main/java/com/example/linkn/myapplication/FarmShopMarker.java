7b85c7d151fc7b723a9a9c1fcde88e63cc5a1ec2
linkna <link.nata@gmail.com>
2018-12-23 19:59:20 +0100

    FYF-53 create farm profile

---
+++ app/src/main/java/com/example/linkn/myapplication/FarmShopMarker.java
@@ -0,0 +1,99 @@
+package com.example.linkn.myapplication;
+
+import android.annotation.SuppressLint;
+
+import java.util.StringJoiner;
+
+public class FarmShopMarker {
+
+    static final String CITY = "ort";
+
+    static final String SHOP_NAME = "shopname";
+
+    static final String STREET = "straße";
+
+    static final String STREET_NUMBER = "hausnummer";
+
+    static final String ZIP_CODE = "plz";
+
+    private String id;
+
+    private String shopName;
+
+    private String city;
+
+    private String zipCode;
+
+    private String streetNumber;
+
+    private String street;
+
+    public String getShopName() {
+        return shopName;
+    }
+
+    @SuppressLint("NewApi")
+    public String getAddressString() {
+
+        return new StringJoiner(" ")
+                .add(street == null ? "" : street)
+                .add(streetNumber == null ? "" : streetNumber)
+                .add(zipCode == null ? "" : zipCode)
+                .add(city == null ? "" : city)
+                .toString();
+    }
+
+    public FarmShopMarker(String id, String shopName, String city, String zipCode, String streetNumber, String street) {
+
+        this.id = id;
+        this.shopName = shopName;
+        this.city = city;
+        this.zipCode = zipCode;
+        this.streetNumber = streetNumber;
+        this.street = street;
+    }
+
+    public static class FarmShopMarkerBuilder {
+
+        private String id;
+        private String shopName;
+        private String city;
+        private String zipCode;
+        private String streetNumber;
+        private String street;
+
+        public FarmShopMarkerBuilder setId(String id) {
+            this.id = id;
+            return this;
+        }
+
+        public FarmShopMarkerBuilder setShopName(String shopName) {
+            this.shopName = shopName;
+            return this;
+        }
+
+        public FarmShopMarkerBuilder setZipCode(String zipCode) {
+            this.zipCode = zipCode;
+            return this;
+        }
+
+        public FarmShopMarkerBuilder setStreetNumber(String streetNumber) {
+            this.streetNumber = streetNumber;
+            return this;
+        }
+
+        public FarmShopMarkerBuilder setStreet(String street) {
+            this.street = street;
+            return this;
+        }
+
+        public FarmShopMarkerBuilder setCity(String city) {
+            this.city = city;
+            return this;
+        }
+
+        public FarmShopMarker build() {
+            return new FarmShopMarker(id, shopName, city, zipCode, streetNumber, street);
+        }
+    }
+}
--- app/src/main/java/com/example/linkn/myapplication/MapsActivity.java
+++ app/src/main/java/com/example/linkn/myapplication/MapsActivity.java
@@ -5,25 +5,24 @@ import android.annotation.SuppressLint;
 import android.content.Context;
 import android.content.Intent;
 import android.content.pm.PackageManager;
+import android.location.Address;
+import android.location.Geocoder;
 import android.location.Location;
 import android.location.LocationListener;
 import android.location.LocationManager;
 import android.os.Bundle;
 import android.support.annotation.NonNull;
 import android.support.v4.app.ActivityCompat;
-import android.support.v4.app.FragmentActivity;
 import android.support.v4.content.ContextCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.util.Log;
 import android.view.View;
 import android.widget.TextView;
 
-
 import com.google.android.gms.maps.CameraUpdateFactory;
 import com.google.android.gms.maps.GoogleMap;
 import com.google.android.gms.maps.OnMapReadyCallback;
 import com.google.android.gms.maps.SupportMapFragment;
-import com.google.android.gms.maps.model.BitmapDescriptor;
 import com.google.android.gms.maps.model.BitmapDescriptorFactory;
 import com.google.android.gms.maps.model.LatLng;
 import com.google.android.gms.maps.model.Marker;
@@ -35,77 +34,40 @@ import com.google.firebase.auth.FirebaseUser;
 import com.google.firebase.firestore.DocumentReference;
 import com.google.firebase.firestore.DocumentSnapshot;
 import com.google.firebase.firestore.FirebaseFirestore;
-import com.google.firebase.firestore.QueryDocumentSnapshot;
 import com.google.firebase.firestore.QuerySnapshot;
 
-
+import java.io.IOException;
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Optional;
+import java.util.function.Function;
+import java.util.stream.Collectors;
 
 
 public class MapsActivity extends AppCompatActivity implements OnMapReadyCallback {
+
+    public static final String FARMSHOP = "Farmshop";
 
     private LocationManager locationManager;
     private LocationListener locationListener;
     private GoogleMap mMap;
     private Marker yourLocation;
     private FirebaseFirestore mDatabase;
-    ArrayList<String> koordinaten=new ArrayList<>();
+    ArrayList<String> koordinaten = new ArrayList<>();
     public FirebaseAuth auth;
     FirebaseUser user = FirebaseAuth.getInstance().getCurrentUser();
-    TextView adressTextView,phoneTextView;
-
-
+    TextView adressTextView, phoneTextView;
 
 
     @Override
     public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
         super.onRequestPermissionsResult(requestCode, permissions, grantResults);
 
-        if(grantResults.length > 0 && grantResults [0] == PackageManager.PERMISSION_GRANTED){
-            if(ContextCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION) == PackageManager.PERMISSION_GRANTED){
+        if (grantResults.length > 0 && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
+            if (ContextCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION) == PackageManager.PERMISSION_GRANTED) {
                 locationManager.requestLocationUpdates(LocationManager.GPS_PROVIDER, 50, 0, locationListener);
             }
         }
-    }
-
-    public void getFarmshops(){
-        mDatabase.collection("Farmshop")
-                .get()
-                .addOnCompleteListener(new OnCompleteListener<QuerySnapshot>() {
-                    @Override
-                    public void onComplete(@NonNull Task<QuerySnapshot> task) {
-                        if (task.isSuccessful()) {
-                            for (QueryDocumentSnapshot document : task.getResult()) {
-
-                                koordinaten.add(document.getString("ort")+"+"+document.getString("straße")+"+"+document.getString("hausnummer")+"+"+document.getString("plz"));
-                                //Log.d(TAG, document.getId() + " => " + document.getData());
-                            }
-                        } else {
-                            //Log.w(TAG, "Error getting documents.", task.getException());
-                        }
-                    }
-                });
-        mDatabase.collection("Farmshop")
-                .get()
-                .addOnCompleteListener(new OnCompleteListener<QuerySnapshot>() {
-                    @Override
-                    public void onComplete(@NonNull Task<QuerySnapshot> task) {
-                        if (task.isSuccessful()) {
-                            int i=0;
-                            for (QueryDocumentSnapshot document : task.getResult()) {
-
-                                koordinaten.add(document.getString("name")+"+"+document.getString("ort")+"+"+document.getString("straße")+"+"+document.getString("hausnummer")+"+"+document.getString("plz"));
-                                //Log.d(TAG, document.getId() + " => " + document.getData());
-                                System.out.println(koordinaten.get(i).toString());
-                                i++;
-                            }
-                        } else {
-                            //Log.w(TAG, "Error getting documents.", task.getException());
-                        }
-                    }
-                });
-
     }
 
     @Override
@@ -116,31 +78,86 @@ public class MapsActivity extends AppCompatActivity implements OnMapReadyCallbac
         auth = FirebaseAuth.getInstance();
         SupportMapFragment mapFragment = (SupportMapFragment) getSupportFragmentManager().findFragmentById(R.id.map);
         mapFragment.getMapAsync(this);
-       getFarmshops();
+
     }
 
+    /**
+     * Transformiert DatankresultSet zu FarmerShopMarker Objekt um eine Abstraktionsebene für
+     * das Mapping zu schaffen und den passenden Adress String zu generieren.
+     * <p>
+     * Hierfür ist in der Klasse FarmShopMarker ein Builder implementiert der das setzen von
+     * Attributen schöner gestaltet (@see com.example.linkn.myapplication.FarmShopMarker.FarmShopMarkerBuilder).
+     */
+    private Function<DocumentSnapshot, FarmShopMarker> mapQueryDocumentSnapshotToFarmshopMarker() {
 
+        return documentSnapshot -> new FarmShopMarker.FarmShopMarkerBuilder()
+                .setId(documentSnapshot.getId())
+                .setShopName(documentSnapshot.getString(FarmShopMarker.SHOP_NAME))
+                .setCity(documentSnapshot.getString(FarmShopMarker.CITY))
+                .setStreet(documentSnapshot.getString(FarmShopMarker.STREET))
+                .setStreetNumber(documentSnapshot.getString(FarmShopMarker.STREET_NUMBER))
+                .setZipCode(documentSnapshot.getString(FarmShopMarker.ZIP_CODE))
+                .build();
+    }
+
+    @SuppressLint("NewApi")
+    private Optional<Address> getAddressByFarmShopMarker(FarmShopMarker farmShopMarker) {
+
+        Geocoder geocoder = new Geocoder(getApplicationContext());
+
+        Optional<Address> address = Optional.empty();
+
+        try {
+            address = Optional.ofNullable(geocoder.getFromLocationName(farmShopMarker.getAddressString(), 1).get(0));
+        } catch (IOException | IndexOutOfBoundsException e) {
+            Log.i("FarmShopMarker", "Can not find address with geocode for farmshop marker adress: " + farmShopMarker.getAddressString());
+        }
+
+        return address;
+    }
+
+    @SuppressLint("NewApi")
     @Override
     public void onMapReady(GoogleMap googleMap) {
+
         mMap = googleMap;
 
+        FarmShopMarkerActivity farmShopMarkerActivity = new FarmShopMarkerActivity();
+        mMap.setOnMarkerClickListener(farmShopMarkerActivity);
 
-        mMap.addMarker(new MarkerOptions()
-                .position(new LatLng(49.029090,  8.461969))
-                .title("Hagsfelder Hofladen")
-                .icon(BitmapDescriptorFactory.defaultMarker(BitmapDescriptorFactory.HUE_GREEN)));
-      //          .icon(BitmapDescriptorFactory.fromResource(R.drawable.marker)));
+        Task<QuerySnapshot> farmShopDatabaseTask = mDatabase.collection(FARMSHOP).get();
 
+        farmShopDatabaseTask.addOnSuccessListener(queryDocumentSnapshots -> {
 
+            List<FarmShopMarker> farmShopMarkers = queryDocumentSnapshots.getDocuments()
+                    .stream()
+                    .map(mapQueryDocumentSnapshotToFarmshopMarker())
+                    .collect(Collectors.toList());
+
+            farmShopMarkers.forEach(farmShopMarker -> {
+                Optional<Address> address = getAddressByFarmShopMarker(farmShopMarker);
+
+                if (address.isPresent()) {
+
+                    MarkerOptions markerOptions = new MarkerOptions()
+                            .position(new LatLng(address.get().getLatitude(), address.get().getLongitude()))
+                            .title(farmShopMarker.getShopName())
+                            .icon(BitmapDescriptorFactory.defaultMarker(BitmapDescriptorFactory.HUE_GREEN));
+
+                    Marker marker = mMap.addMarker(markerOptions);
+                    marker.setTag(farmShopMarker);
+                }
+            });
+        });
 
         locationManager = (LocationManager) this.getSystemService(Context.LOCATION_SERVICE);
         locationListener = new LocationListener() {
             @Override
             public void onLocationChanged(Location location) {
-              //  mMap.clear();
+                //  mMap.clear();
 
-                if(yourLocation!=null&&yourLocation.isVisible()){
-                  yourLocation.setVisible(false);
+                if (yourLocation != null && yourLocation.isVisible()) {
+                    yourLocation.setVisible(false);
                 }
                 LatLng userLocation = new LatLng(location.getLatitude(), location.getLongitude());
 
@@ -148,7 +165,7 @@ public class MapsActivity extends AppCompatActivity implements OnMapReadyCallbac
                         .position(userLocation)
                         .title("aktueller Standort"));
 
-              //mMap.addMarker(new MarkerOptions().position(userLocation).title("Your Location"));
+                //mMap.addMarker(new MarkerOptions().position(userLocation).title("Your Location"));
                 mMap.moveCamera(CameraUpdateFactory.newLatLngZoom(userLocation, 12));
 
           /*      getFarmshops();
@@ -164,8 +181,6 @@ public class MapsActivity extends AppCompatActivity implements OnMapReadyCallbac
                             .icon(BitmapDescriptorFactory.defaultMarker(BitmapDescriptorFactory.HUE_BLUE)));
                 }
 */
-
-
 
 
             }
@@ -189,14 +204,9 @@ public class MapsActivity extends AppCompatActivity implements OnMapReadyCallbac
         };
 
 
-
-
-
-
-
-        if (ContextCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED){
-            ActivityCompat.requestPermissions( this, new String [] {Manifest.permission.ACCESS_FINE_LOCATION}, 1);
-        }else{
+        if (ContextCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
+            ActivityCompat.requestPermissions(this, new String[]{Manifest.permission.ACCESS_FINE_LOCATION}, 1);
+        } else {
             //minTime legt fest, wie oft die Location aktualisiert wird, pro sekunde, halbe Minute usw.
             locationManager.requestLocationUpdates(LocationManager.GPS_PROVIDER, 3, 0, locationListener);
         }
@@ -204,7 +214,8 @@ public class MapsActivity extends AppCompatActivity implements OnMapReadyCallbac
 
 
     }
-    public void goToFarmshop(View view){
+
+    public void goToFarmshop(View view) {
 
         setContentView(R.layout.farm_shop_profile);
         adressTextView = (TextView) findViewById(R.id.adressTextView);
@@ -214,14 +225,14 @@ public class MapsActivity extends AppCompatActivity implements OnMapReadyCallbac
         //gibt den String des aktuellen Users
         String uid = user.getUid();
 
-        DocumentReference Farmshop=mDatabase.collection("Farmshop").document(uid);
+        DocumentReference Farmshop = mDatabase.collection("Farmshop").document(uid);
 
-        Farmshop.get().addOnCompleteListener(new OnCompleteListener<DocumentSnapshot>(){
+        Farmshop.get().addOnCompleteListener(new OnCompleteListener<DocumentSnapshot>() {
             public void onComplete(@NonNull Task<DocumentSnapshot> task) {
 
-                DocumentSnapshot document=task.getResult();
-                String adresse=document.getString("straße")+" "+document.getString("hausnummer")+" "+document.getString("plz")+" "+document.getString("ort");
-                String phone=document.getString("phone");
+                DocumentSnapshot document = task.getResult();
+                String adresse = document.getString("straße") + " " + document.getString("hausnummer") + " " + document.getString("plz") + " " + document.getString("ort");
+                String phone = document.getString("phone");
                 // String geb=document.getString("born");
                 adressTextView.setText(adresse);
                 phoneTextView.setText(phone);
@@ -230,13 +241,14 @@ public class MapsActivity extends AppCompatActivity implements OnMapReadyCallbac
 
 
     }
-    public void profilButton(View view){
+
+    public void profilButton(View view) {
 
         FirebaseUser user = FirebaseAuth.getInstance().getCurrentUser();
-        if(user==null){
+        if (user == null) {
             startActivity(new Intent(MapsActivity.this, MainActivity.class));
 
-        }else{
+        } else {
             startActivity(new Intent(MapsActivity.this, Profile.class));
         }
     }
