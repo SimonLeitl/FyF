1f88c53f4d2a323239b5647fe25d2ae7190c50d0
simonleitl <simon.leitl@t-online.de>
2019-04-27 12:09:39 +0200

    creating Unit-Test for Log-In

---
+++ app/src/test/java/com/example/linkn/myapplication/LoginActivityTest.java
@@ -0,0 +1,22 @@
+package com.example.linkn.myapplication;
+
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+public class LoginActivityTest {
+
+    @Test
+    public void onCreate() {
+        String name="test@test.de";
+        String passwort="123456";
+        LoginActivity loginActivity=new LoginActivity();
+
+
+    }
+
+    @Test
+    public void loginZurückButtonClick() {
+
+    }
+}
