27acda023cde4b1240467c2269fefe6500aee979
simonleitl <simon.leitl@t-online.de>
2019-06-18 09:25:06 +0200

    Bug fixes

--- app/src/main/java/com/example/linkn/myapplication/Account.java
+++ app/src/main/java/com/example/linkn/myapplication/Account.java
@@ -1,4 +1,17 @@
 package com.example.linkn.myapplication;
 
 public class Account {
+    private String id,name, nachname, alter;
+    private int geburtstag;
+
+    public Account(){
+
+    }
+    public Account(String name, String nachname, String alter, String id){
+        this.name=name;
+        this.nachname=nachname;
+        this.alter=alter;
+        this.id=id;
+    }
+
 }
